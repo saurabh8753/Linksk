27b59092f0908e1520d1ce83c87772469f5c2316
saurabh8753 <130946713+saurabh8753@users.noreply.github.com>
2023-09-07 18:50:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
+API_ID       = 24286057
+API_HASH     = "9ab62f8268f1c5a60b2808f06f2ad2c9"
+BOT_TOKEN    = "6543802429:AAGfd93pFN-xUtSjGOJ9YJgn2O9-he6uePw"
 SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
+DATABASE_URI = "mongodb+srv://saurabhbankar32323:KlwI1AbnyunPzOEA@cluster0.xgsn1ex.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001954311684
 ADMIN        = 1350642629
 CHANNEL      = "@GreyMatter_Bots"
